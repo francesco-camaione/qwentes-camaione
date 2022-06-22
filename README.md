a44de9b8e79a3c820b068336976812d09237025b
francesco-camaione <francesco.camaione@gmail.com>
2022-06-22 16:36:30 +0200

    first commit

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
  To execute this app: 
 
+1.
+
     docker build . -t qwentes_camaione
 
+2.
+
     docker run -p 3000:3000 qwentes_camaione
+3.
 
     open localhost:3000 on your browser
--- src/services/getData.ts
+++ src/services/getData.ts
@@ -1,4 +1,6 @@
-export async function getData(target: string) {
+type Target = "users" | "posts"
+
+export async function getData(target: Target) {
     // get contacts or posts based on parameter - target should be: users or posts 
     const url = `https://jsonplaceholder.typicode.com/${target}`
     const response = await fetch(url, {
